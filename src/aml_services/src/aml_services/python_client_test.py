8a62f6f393a2c0b7671c41920bacc58baed8fb79
justagist <saifksidhik@gmail.com>
2018-03-11 16:45:20 +0000

    services for computing surface parameters, curvature and normals for all points;

--- src/aml_services/src/aml_services/python_client_test.py
+++ src/aml_services/src/aml_services/python_client_test.py
@@ -9,8 +9,9 @@ from aml_services.msg import PCLCustomMsg
 
 if __name__ == "__main__":
 
-
+    print "waiting for service"
     rospy.wait_for_service('aml_pcl_service')
+    print "service found"
     try:
         client = rospy.ServiceProxy('aml_pcl_service', PCLUtility)
 
@@ -24,29 +25,41 @@ if __name__ == "__main__":
         print resp.info
 
         raw_input()
-        
+
+
         try:
-            # print resp.out_cloud_1
             msg2 = PCLCustomMsg()
             msg2.cloud_1 = resp.msg_out.cloud_1
-            msg2.float_array_1 = [0.01,0.01,0.01]
-            # resp2 = client("downsample_cloud",resp.msg_out.cloud_1,None,None,[0.01,0.01,0.01])
-            resp2 = client("downsample_cloud",msg2)
+            resp2 = client("compute_all_normals", msg2)
 
             print resp2.info
 
-            raw_input()
+            # print resp2.msg_out.cloud_1
+            # print resp2.msg_out.float_1
 
-            try:
-                msg3 = PCLCustomMsg()
-                msg3.cloud_1 = resp2.msg_out.cloud_1
-                msg3.string_1 = "/home/saif/Desktop/image_0003.pcd"
-                resp3 = client("save_to_file",msg3)
+        
+        # try:
+        #     # print resp.out_cloud_1
+        #     msg2 = PCLCustomMsg()
+        #     msg2.cloud_1 = resp.msg_out.cloud_1
+        #     msg2.float_array_1 = [0.01,0.01,0.01]
+        #     # resp2 = client("downsample_cloud",resp.msg_out.cloud_1,None,None,[0.01,0.01,0.01])
+        #     resp2 = client("downsample_cloud",msg2)
 
-                print resp3.info
+        #     print resp2.info
 
-            except rospy.ServiceException, e:
-                print "Service call failed: %s"%e
+        #     raw_input()
+
+        #     try:
+        #         msg3 = PCLCustomMsg()
+        #         msg3.cloud_1 = resp2.msg_out.cloud_1
+        #         msg3.string_1 = "/home/saif/Desktop/image_0003.pcd"
+        #         resp3 = client("save_to_file",msg3)
+
+        #         print resp3.info
+
+        #     except rospy.ServiceException, e:
+        #         print "Service call failed: %s"%e
 
         except rospy.ServiceException, e:
             print "Service call failed: %s"%e
